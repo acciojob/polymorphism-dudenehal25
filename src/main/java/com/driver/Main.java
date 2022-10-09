3c9541356cb4284ed24593581b51ad40a73327d5
dudenehal25 <dudenehal25@gmail.com>
2022-10-09 12:47:32 +0530

    done first task

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(2,4));
+        System.out.println(p.product(2,3,4));
+        System.out.println(p.product(2.2,4.3));
+    }
+
+    static class Product{
+
+        public int product(int a,int b){
+            return a*b;
+        }
+
+        public int product(int a,int b,int c){
+            return a*b*c;
+        }
+
+        public double product(double x , double y){
+            return x*y;
+        }
+
+    }
+
 }
+
